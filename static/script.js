4a9e18bbb6484fb06828bb9048edeb97acad641e
fuckzulrah <jchen123556@gmail.com>
2023-04-08 17:27:16 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,20 @@
-async function testFuncTwo() { 
-    fetch("/companies/company1&company").then(async(res) => document.getElementById('testDiv').innerHTML = await res.text());  
-    fetch("thing").then(async(res) => { 
+async function testFuncTwo(companyString) { 
+    fetch(`/companies/${companyString.substring(1)}`).then(async(res) => { 
         const json = JSON.parse(await res.text())
         const numberOfCompanies = Object.keys(json).length; 
+        const totalScore = 0; 
+        const totalApproachScore = 0; 
+
 
         for (let i of Object.keys(json)) { 
-
+            totalScore += json[i]["totalScore"] / numberOfCompanies; 
+            totalApproachScore += json[i]["approachScore"] / numberOfCompanies; 
         }
-        document.createElement('h1')
+        document.createElement('h1').innerText = `AVERAGES -- TOTAL SCORE: ${totalScore}  APPROACH SCORE: ${approachScore}`; 
+
+        for (let i of Object.keys(json)) { 
+            document.createElement('h2').innerText = `${i} -- TOTAL SCORE: ${json[i]["totalScore"]}  APPROACH SCORE: ${json[i]["approachScore"]}}`;
+        }
     }); 
 }
 
