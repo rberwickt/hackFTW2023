e2f5c318e10308e7fcb84db374e0e13a0f8c8ff3
rberwickt <rtberwick@gmail.com>
2023-04-08 17:05:38 -0500

    Local Saving

--- static/form.js
+++ static/form.js
@@ -1,9 +1,8 @@
 //ideally we generate radio buttons off of their input in order to have them choose the company to add
-sessionStorage.setItem("list",[]);
+
 const htmlForm = document.getElementById("cform");
 const cInput = document.getElementById("cname");
 const radioDiv = document.getElementById("radioDiv");
-sessionStorage.setItem("list", []);
 async function searchComp(){
     let value = cInput.value;
     let array; 
@@ -34,7 +33,16 @@ async function searchComp(){
         radioDiv.appendChild(label);
         radioDiv.appendChild(document.createElement("br"));
     }
-    
-    
-
 }
+
+htmlForm.addEventListener("submit", (event)=>{
+    const data = new FormData(htmlForm);
+    let output = "";
+    for (const entry of data) {
+      output = entry[1];
+    }
+    localStorage.setItem("compList", localStorage.getItem("compList") + "&" + output);
+    console.log(localStorage.getItem("compList"));
+    location.reload();
+    
+})
